ba681a094efaeaaa34e3cbaccd29f856f2ff06ac
abigailarteaga <abigailartecar@hotmail.com>
2025-07-10 21:50:52 -0500

    puerto

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const PORT = process.env.PORT || 3043;
+const PORT = 3043;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
